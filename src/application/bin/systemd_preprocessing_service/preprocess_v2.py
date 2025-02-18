1f71569fb0251821fe4282464e8e01c4ff2bce31
Akim Malyshchyk <akim.malyshchyk@qorpo.world>
2025-02-18 13:54:00 +0100

    cleanup preprocessing scripts

--- src/application/bin/systemd_preprocessing_service/preprocess_v2.py
+++ src/application/bin/systemd_preprocessing_service/preprocess_v2.py
@@ -1,12 +1,12 @@
 import json
-import asyncio
 from pathlib import Path
-from application.dtos.game_state_event import GameStateGroupGameEvent, GameStateEventData, GameState
-from application.db_models import GameState as DBGameState, DatabaseConnection
+
+from application.db_models import DatabaseConnection
+from application.db_models import GameState as DBGameState
+from application.dtos.game_state_event import GameStateGroupGameEvent
 
 
-
-async def run_preprocess(json_path):
+async def run_preprocess(json_path: Path) -> None:
     # Step 1: Load playground.json
     print("i am here")
     if not json_path.exists():
@@ -14,22 +14,18 @@ async def run_preprocess(json_path):
         return
 
     with open(json_path, "r") as file:
-        raw_data = json.load(file)
+        raw_data: dict = json.load(file)
 
     db = DatabaseConnection()
     # Step 2: Convert raw data to GameStateEvent
     for event_dict in raw_data:
-        event_dict: dict
         game_state_event: GameStateGroupGameEvent = GameStateGroupGameEvent(**event_dict)
-
 
         session = db.get_session()
         for event in game_state_event.event_data.game_states:
-            event: GameState
-
             focused_players = []
             for player in event.players:
-                if player.user_id == '83248802-90e1-705c-8702-e6c497b686d4':
+                if player.user_id == "83248802-90e1-705c-8702-e6c497b686d4":
                     focused_players.append(player)
 
             if not focused_players:
@@ -37,17 +33,14 @@ async def run_preprocess(json_path):
 
             event.players = focused_players
 
-
             event_data_dict = event.model_dump()
 
-            db_model = DBGameState(
-                event_created_at=event.created_at,
-                event_data=json.dumps(event_data_dict)
-            )
+            db_model = DBGameState(event_created_at=event.created_at, event_data=json.dumps(event_data_dict))
 
             session.add(db_model)
 
         session.commit()
+
 
 # Entry point for the script
 # if __name__ == "__main__":
--- src/application/bin/systemd_preprocessing_service/utils.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def get_game_events_from_json(json_filename: str) -> list[dict]:
-    with open(json_filename) as f:
-        return json.load(f)["samples"]
